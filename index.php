9a438d1c631025001db5b26fe53e4715fa7744c5
jrdavij <ryuuji.aisaka7@gmail.com>
2020-10-06 20:54:54 -0300

    major body
    formularios, css

--- index.php
+++ index.php
@@ -1,8 +1,94 @@
+<html>
+    <head>
+        <title>Cadastro de clientes</title>
+        
+        <style>
+            html, body{
+                width: 100%;
+                height: 100%;
+                margin: 0px;
+            }
+            .top{
+                background-color: #A52A2A;
+                font-size: 20px;
+                color: white;
+                padding: 10px;
+                font-family: Source Sans Pro,arial,sans-serif;
+            }
+            .card{
+                margin:auto;
+                width: 400px;
+                border-radius: 4px;
+                padding: 42px;
+                box-shadow: 1px 1px 4px #ccc;
+                font-family: Source Sans Pro, arial, sans-serif;
+                color: #555;
+            }
+            .bottom{
+                background-color: #A52A2A;
+                color: white;
+                padding: 10px;
+                font-family: Source Sans Pro,arial,sans-serif;
+            }
+            label{
+                color: #555;
+                font-weigth: 600;
+                font-size: 14px;
+            }
+            input{
+                width: 100%;
+                height: 31px;
+                margin-top: 5px;
+                margin-bottom: 10px;
+                padding: 6px 12px;
+                font-size: 15px;
+                color: #555;
+                border: 1px solid #ccc;
+                border-radius: 3px;
+            }
+            input:focus{
+                outline: none;
+                box-shadow: 0px 0px 5px skyblue;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="top">Nome do site</div>
+        <div style="padding:30px; margin:auto">
+            <div class="card">
+                <h2>Crie sua conta</h2>
+                <br>
+                <form>
+                    <label>Nome completo:</label>
+                    <input type="text">
+                    <label>E-mail:</label>
+                    <input type="email">
+                    <label>Senha:</label>
+                    <input type="password">
+                    <label>CPF:</label>
+                    <input type="number">
+                    <label>Endereço:</label>
+                    <input type="text">
+                    <label>Bairro:</label>
+                    <input type="text">
+                    <label>Número:</label>
+                    <input type="number">
+                    <label>Cidade:</label>
+                    <input type="text">
+                    <label>Estado:</label>
+                    <input type="text">
+                    <label>CEP:</label>
+                    <input type="number">
+                </form>
+            </div>
+        </div>
+        <div class="bottom">Contato</div>
+    </body>
+    
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+//
+?>
+</html>
 
